25302dd1773c8e4e913961349586d51944bdf1ff
David Bucur <dbucur22@gmail.com>
2022-05-04 22:34:53 +0300

    added search by VIN

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import axios from "axios";
+export default function App() {
+  const [search, setSearch] = React.useState("");
+  const [transactions, setTransactions] = React.useState(null);
+  const getAssociations = () => {
+    axios
+      .get("https://devnet-gateway.vehicknetwork.com/api/carVin/" + search)
+      .then((response) => {
+        setTransactions(response.data);
+      });
+  };
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Vim setup</h1>
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <h1>Search car by Vin</h1>
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      <button onClick={getAssociations}>Search</button>
+      {!transactions ? (
+        <p>No results</p>
+      ) : (
+        <div>
+          {transactions.map((tx: any, i: number) => (
+            <p key={i}>{tx.txHash}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
-export default App;
