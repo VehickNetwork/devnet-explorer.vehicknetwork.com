aba809b5183e4a44da9572a2851bc2cc429ca1e7
David Bucur <dbucur22@gmail.com>
2022-05-06 12:09:18 +0300

    added search by address or vin

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import axios from "axios";
 export default function App() {
   const [search, setSearch] = React.useState("");
   const [transactions, setTransactions] = React.useState(null);
-  const getAssociations = () => {
+  const [measureUnit, setMeasureUnit] = React.useState("");
+  const getTransactionsByVin = () => {
     axios
       .get("https://devnet-gateway.vehicknetwork.com/api/carVin/" + search)
+      .then((response) => {
+        setTransactions(response.data);
+      });
+  };
+  const getTransactionsByAddress = () => {
+    axios
+      .get("https://devnet-gateway.vehicknetwork.com/api/carAddress/" + search)
       .then((response) => {
         setTransactions(response.data);
       });
@@ -15,11 +23,28 @@ export default function App() {
     <div className="p-4">
       <h1>Search car by Vin</h1>
       <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      <button onClick={getAssociations}>Search</button>
+      <button
+        onClick={
+          search.length == 62 && search.includes("erd")
+            ? getTransactionsByAddress
+            : getTransactionsByVin
+        }
+      >
+        Search
+      </button>
       {!transactions ? (
         <p>No results</p>
       ) : (
         <div>
+          <h1>Car VIN: {transactions[transactions.length - 1].actionValue}</h1>
+          <div className="flex p-4">
+            <h1 className="pr-2">Current mileage:</h1>
+            <h1 className="pr-2">{transactions[0].actionValue}</h1>
+            <h1 className="pr-2">
+              {transactions[transactions.length - 2].actionValue}
+            </h1>
+          </div>
+          <h1 className="font-bold">History:</h1>
           {transactions.map((tx: any, i: number) => (
             <div className="p-5">
               <h1>Action: {tx.action}</h1>
