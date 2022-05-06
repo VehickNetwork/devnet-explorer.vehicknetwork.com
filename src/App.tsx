0c2840eca3b724f4b31ace92891edd8d06d4e9c5
David Bucur <dbucur22@gmail.com>
2022-05-06 13:06:02 +0300

    ready for deploy v1

--- src/App.tsx
+++ src/App.tsx
@@ -18,38 +18,71 @@ export default function App() {
         setTransactions(response.data);
       });
   };
-
+  const handleKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      if (search.length == 62 && search.includes("erd")) {
+        getTransactionsByAddress();
+      } else {
+        getTransactionsByVin();
+      }
+    }
+  };
   return (
     <div className="p-4">
-      <h1>Search car by Vin</h1>
-      <input value={search} onChange={(e) => setSearch(e.target.value)} />
-      <button
-        onClick={
-          search.length == 62 && search.includes("erd")
-            ? getTransactionsByAddress
-            : getTransactionsByVin
-        }
-      >
-        Search
-      </button>
+      <h1>Search</h1>
+      <div className="flex items-center justify-center">
+        <input
+          className="form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          value={search}
+          onKeyPress={handleKeyPress}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="inline-block px-4 py-2 font-bold text-white align-middle bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+          onClick={
+            search.length == 62 && search.includes("erd")
+              ? getTransactionsByAddress
+              : getTransactionsByVin
+          }
+        >
+          Search
+        </button>
+      </div>
       {!transactions ? (
         <p>No results</p>
       ) : (
-        <div>
-          <h1>Car VIN: {transactions[transactions.length - 1].actionValue}</h1>
-          <div className="flex p-4">
-            <h1 className="pr-2">Current mileage:</h1>
-            <h1 className="pr-2">{transactions[0].actionValue}</h1>
-            <h1 className="pr-2">
-              {transactions[transactions.length - 2].actionValue}
-            </h1>
+        <div className="px-2 py-4">
+          <div className="p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <h1>VIN: {transactions[transactions.length - 1].actionValue}</h1>
+            <div className="flex pt-4">
+              <h1 className="pr-2">Current mileage:</h1>
+              <h1 className="pr-2">{transactions[0].actionValue}</h1>
+              <h1 className="pr-2">
+                {transactions[transactions.length - 2].actionValue}
+              </h1>
+            </div>
           </div>
-          <h1 className="font-bold">History:</h1>
+          <h1 className="py-4 font-bold">History:</h1>
           {transactions.map((tx: any, i: number) => (
-            <div className="p-5">
+            <div className="p-6 my-4 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
               <h1>Action: {tx.action}</h1>
               <h2>Action Value: {tx.actionValue}</h2>
               <p id={i.toString()}>Tx Hash: {tx.txHash}</p>
+              <p>Date: {tx.timestamp}</p>
             </div>
           ))}
         </div>
